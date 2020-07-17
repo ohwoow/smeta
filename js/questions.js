68b568a61117549e9e3f183ba29b7f930150f29d
ohwoow <59616113+ohwoow@users.noreply.github.com>
2020-07-17 14:21:32 +0500

    Add files via upload

---
+++ js/questions.js
@@ -0,0 +1,50 @@
+const quiz = [{
+        "q": `<div class='modal__question'>На какие виды работ требуется подготовить смету?</div>`,
+        "a": {
+            "b": "Проектные работы",
+            "c": "Пусконаладочные работы",
+            "d": "Строительные работы",
+            "i": "Укрупненный расчет",
+            "f": "Монтажные работы"
+        }
+    },
+    {
+        "q": "<div class='modal__question'>Какие дополнительные документы требуются?</div>",
+        "a": {
+            "e": "Никаких",
+            "b": "Акт по форме КС-2",
+            "c": "Конъюнктурный анализ цен"
+        }
+    },
+    {
+        "q": "<div class='modal__question'>Какая ориентировочная площадь объекта? (кв.м)</div>",
+        "a": {
+            "e": `<div class="slidecontainer">
+                <input type="range" min="1" max="100" value="50" class="slider" id="myRange">
+                <p>Value: <span id="demo"></span></p>
+            </div>`
+        }
+    },
+    {
+        "q": "<div class='modal__question'>Какая ориентировочная площадь объекта? (кв.м)</div>",
+        "a": {
+            "e": `<div class="slidecontainer">
+                <input type="range" min="1" max="100" value="50" class="slider" id="myRange">
+                <p>Value: <span id="demo"></span></p>
+            </div>`
+        }
+    },
+    {
+        "q": "<div class='modal__question'>Требуется ли защита проекта в экспертизе?</div>",
+        "a": {
+            "e": "Да",
+            "b": "Нет"
+        }
+    },
+    {
+        "q": "Отлично! Мы уже начинаем готовить вашу смету. Как нам с вами связаться?",
+        "a": {
+
+        }
+    },
+];
